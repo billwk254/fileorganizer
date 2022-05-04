cbcaabbcc4e65e96b213fed1b7eb6565219d4175
billwk254 <billkihungu51@gmail.com>
2022-05-04 16:03:17 +0300

    Refactored the code into two functions
    Provided more information for the user about what the code does

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 import shutil, os , re
-filepattern = re.compile(r"(y2mate.com - )(.*?)$", re.DOTALL)
 
-while True:
-    filelocation = input("Do you want to use the current directory?(y/n): \n")
-    if filelocation.lower() == "y":
-        loc = os.getcwd()
-        break
-    elif filelocation.lower() == "n":
-        loc = os.path.abspath(input("Please enter a path to be used: \n"))
-        break
-    else:
-        print ("Please Enter y/n \n")
+print("{0:>45}".format("RENAME ALL FILES DOWNLOADED FROM YOUTUBE\n"))
 
-ytfiles = os.listdir(loc)
+location = ""
+
+def take_user_input():
+    """A FUNCTION THAT ASKS THE USER TO PROVIDE A DIRECTORY TO
+    WORK WITH"""
+    while True:
+        filelocationation = input("Do you want to use the current directory?(y/n): \n")
+        if filelocationation.lower() == "y":
+            location = os.getcwd()
+            break
+        elif filelocationation.lower() == "n":
+            #convert the path into an absolute path
+            user_path = os.path.abspath(input("Please enter a path to be used: \n"))
+            #check whether the path exists and is a directory 
+            if os.path.exists(user_path) and os.path.isdir(user_path):
+                location = user_path
+                break
+        else:
+            print ("Please Enter y/n \n")
 
 
-for y2mate in ytfiles:
-    y2 = filepattern.search(y2mate)
-    if y2 == None:
-        continue
-    importantpart = y2.group(1)
-    filenme = y2.group(2)
-    oldfilename = os.path.join(loc ,y2mate)
-    newfilename = os.path.join(loc, filenme)
-    print('Renaming "%s" to "%s"....' %(oldfilename, newfilename))
-    shutil.move(oldfilename , newfilename)
+def rename_files():
+    """A FUNCTION FOR RENAMING ALL YOUTUBE FILES DOWNLOADED FROM Y2MATE.COM
+      """
+      #a regular expression to match files downloaded from y2mate.com
+    filepattern = re.compile(r"(y2mate.com - )(.*?)$", re.DOTALL)
+    #list all files in the provided location
+    ytfiles = os.listdir(location)
+#loop through all files in the directory
+    for y2mate in ytfiles:
+        #search for files with the pattern in the regular expression
+        y2 = filepattern.search(y2mate)
+        if y2 == None:
+            continue
+        #store both parts of the filename in variables 
+        importantpart = y2.group(1)
+        filenme = y2.group(2)
+        #join the two filenames with the directory path
+        oldfilename = os.path.join(location ,y2mate)
+        newfilename = os.path.join(location, filenme)
+        print('Renaming "%s" to "%s"....' %(oldfilename, newfilename))
+        #use the shutil module to rename the files
+        shutil.move(oldfilename , newfilename)
 
 
 
